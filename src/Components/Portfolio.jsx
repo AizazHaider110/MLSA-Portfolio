800e62da402e37e590a3c7256b8e104a37f643ea
AizazHaider110 <87595565+AizazHaider110@users.noreply.github.com>
2023-11-14 06:21:18 +0000

    hellpo

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Aizaz.jpg";
+import image from "../images/Aizaz.JPG";
 
 const imageAltText = "desktop with books and laptop";
 
